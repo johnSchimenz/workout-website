37fa4797348a627480c78f9d610e940fc7dffdc9
John Schimenz <john.schimenz@gmail.com>
2023-12-12 07:30:33 -0600

    Adds new exercise to workout container, but display doesn't look good

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // Add exercise
-const addNewExercise = document.querySelector('#workout-container');
-addNewExercise.addEventListener('click', {
-    
+const addNewExercise = document.querySelector('#add-exercise');
+const selectWorkoutContainer = document.querySelector('#workout-container')
+addNewExercise.addEventListener('click', () => {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'exercise');
+    div.textContent = 'New Exercise';
+    selectWorkoutContainer.appendChild(div);
 })
